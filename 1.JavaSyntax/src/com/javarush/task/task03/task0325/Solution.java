66c6f7f8cb21fe2fd388af06c8b8b7e00d34e4c9
mr.Boogie <zik2004@mail.ru>
2021-11-13 14:34:31 +0300

    task 0325 - finish

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0325/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task03.task0325;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Финансовые ожидания
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Я буду зарабатывать $"+ br.readLine() +" в час");
+    }
+}
