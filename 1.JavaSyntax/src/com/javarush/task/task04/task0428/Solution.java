238d253baab840291011128cb4c6556dd210fd72
mr.Boogie <zik2004@mail.ru>
2021-11-20 22:27:33 +0300

    task 0428 - finish

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0428/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task04.task0428;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Положительное число
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+        int num3 = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        if (num1>0) count++;
+        if (num2>0) count++;
+        if (num3>0) count++;
+        System.out.println(count);
+
+    }
+}
