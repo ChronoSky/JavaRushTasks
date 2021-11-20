eac22cf8f4b62637c8164ee02c406251fad4a2d1
mr.Boogie <zik2004@mail.ru>
2021-11-20 16:13:51 +0300

    task 0418 - finish

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0418/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task04.task0418;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Минимум двух чисел Ӏ Java Syntax: 4 уровень, 6 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+
+        if (num1 < num2 || num1 == num2){
+            System.out.println(num1);
+        }else {
+            System.out.println(num2);
+        }
+
+    }
+}
