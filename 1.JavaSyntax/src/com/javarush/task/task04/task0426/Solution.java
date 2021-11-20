ec7f72889280ea1d975e238f4758c5b10d79a470
mr.Boogie <zik2004@mail.ru>
2021-11-20 22:24:13 +0300

    task 0426 - finish

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0426/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task04.task0426;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Ярлыки и числа
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num= Integer.parseInt(br.readLine());
+
+        if (num < 0){
+            if (num%2==0){
+                System.out.println("отрицательное четное число");
+            }else{
+                System.out.println("отрицательное нечетное число");
+            }
+        }else if(num > 0){
+            if (num%2==0){
+                System.out.println("положительное четное число");
+            }else{
+                System.out.println("положительное нечетное число");
+            }
+        }else {
+            System.out.println("ноль");
+        }
+
+    }
+}
