31d21241e926609fca99bf4736402fe4d84ce36b
mr.Boogie <zik2004@mail.ru>
2021-11-25 18:11:29 +0300

    task 0441 - finish

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0441/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task04.task0441;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Как-то средненько
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+        int num3 = Integer.parseInt(br.readLine());
+
+        int max, min, avg;
+
+        // определили минимальное и максимально между 1 и 3
+        max = Math.max(num1, num2);
+        min = Math.min(num1, num2);
+
+        if (num3>max){
+            avg = max;
+            max = num3;
+        } else if (num3<min){
+            avg = min;
+            min = num3;
+
+        } else {
+            avg = num3;
+        }
+
+        System.out.println(avg);
+    }
+}
