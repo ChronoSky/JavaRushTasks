198f696ce1a1246a0f4b8ba62e69f452c6d9e933
ChronoSky <zik2004@mail.ru>
2022-10-27 00:20:17 +0300

    task 1208 - finish

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int num){}
+    public static void print(Integer num){}
+    public static void print(String num){}
+    public static void print(Double num){}
+    public static void print(Float num){}
+
+
+}
